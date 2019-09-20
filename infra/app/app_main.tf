236a154538decece11c3ec422f4dee20f7a55cb0
Shreya.Chatterjee <shreya.chatterjee@ShreyaChatterjees-MacBook-Pro.local>
2019-09-20 14:57:38 +0200

    till ec2 jenkins set up

---
+++ infra/app/app_main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "blue-jenkins-server" {
+  source = "../modules/compute"
+  #server_port = var.server_port
+  security_group_name=module.security.security_group_name
+}
+
+module "security" {
+  source = "../modules/security"
+  from_port = var.server_port
+  to_port = var.server_port
+}
